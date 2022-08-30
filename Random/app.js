3769f9f6d560035c2cd936d2e4c169271c1a6520
伊東聖矢 <itoseiya1219@gmail.com>
2022-08-30 15:15:22 +0900

    Random Button

---
+++ Random/app.js
@@ -0,0 +1,21 @@
+const elem = document.querySelector("#num")
+let intro = null;
+
+document.getElementById("start-btn").addEventListener("click", function(e){
+    if(e.cancelable) {
+        // デフォルトの挙動をキャンセル
+        e.preventDefault();
+    }
+
+    intro = setInterval(function() {
+        elem.textContent = Math.floor(Math.random() * 101);
+    },100);
+});
+
+document.getElementById("stop-btn").addEventListener("click", function(e){
+    if(e.cancelable) {
+        // デフォルトの挙動をキャンセル
+        e.preventDefault();
+    }
+    clearInterval(intro);
+});
